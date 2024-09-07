a4b438fac9dbeefe645f36de2722c5efe571b412
abdulrahman-2 <abdoattallah3@gmail.com>
2024-09-07 17:20:29 +0300

    Build the arch of project and add all packages

---
+++ src/pages/ChefDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChefDashboard = () => {
+  return <div>ChefDashboard</div>;
+};
+
+export default ChefDashboard;
---
+++ src/pages/ChefProfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChefProfile = () => {
+  return <div>ChefProfile</div>;
+};
+
+export default ChefProfile;
