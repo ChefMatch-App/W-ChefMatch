e284bf2a4661a5e11926401d46c96197d83c4e1e
abdulrahman-2 <abdoattallah3@gmail.com>
2024-09-08 02:23:00 +0300

    add signUp and Login To App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/layout/Header";
 import Notification from "./components/layout/Notification";
 import Home from "./pages/home/Home";
 import Footer from "./components/layout/Footer";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
       <div className="container px-4">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/" element={<SignUp />} />
+          <Route path="/" element={<Login />} />
         </Routes>
         <Notification />
       </div>
